9d2cfe974f408fe3059baf9cc262b2ae6c60f86b
Sarah Delfino <sarahdelfino7@gmail.com>
2026-02-11 17:29:43 -0500

    redesign

--- src/app/my-preset.ts
+++ src/app/my-preset.ts
@@ -1,10 +1,11 @@
-import { definePreset } from '@primeng/themes';
-import Nora from '@primeng/themes/nora';
+import { definePreset } from '@primeuix/themes';
+import Nora from '@primeuix/themes/nora';
 
 export const MyPreset = definePreset(Nora, {
   components: {
     carousel: {
       indicator: {
+        background: '#500909',
         activeBackground: '#c9a14a'
       }
     }
