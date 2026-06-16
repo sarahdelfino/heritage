e393c4c9005177ea5578c5b229a9890155d93241
Sarah Delfino <sarahdelfino7@gmail.com>
2026-06-16 12:24:22 -0400

    adjusted carousels

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
     apiKey: "AIzaSyBqGfdp2sYKLihqGFfqcIPz2qBb-CnLqbg",
@@ -15,6 +16,7 @@ const firebaseConfig = {
 }
 
 export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app, 'us-central1');
